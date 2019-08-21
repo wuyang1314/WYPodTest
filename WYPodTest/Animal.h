5b0f02f5d75c2dcdbd3605aa06375c104009727f
liwuyang <liwuyang@lbesec.com>
2019-08-21 17:24:42 +0800

    xx

---
+++ WYPodTest/Animal.h
@@ -0,0 +1,17 @@
+//
+//  Animal.h
+//  WYPodTest
+//
+//  Created by lbe on 2019/8/21.
+//  Copyright © 2019 liwuyang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Animal : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ WYPodTest/Animal.m
@@ -0,0 +1,13 @@
+//
+//  Animal.m
+//  WYPodTest
+//
+//  Created by lbe on 2019/8/21.
+//  Copyright © 2019 liwuyang. All rights reserved.
+//
+
+#import "Animal.h"
+
+@implementation Animal
+
+@end
